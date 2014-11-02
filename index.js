342a0f41ca91a589a6dd71d7f4c989da2166241d
Mathias Buus <mathiasbuus@gmail.com>
2014-11-02 10:28:16 +1000

    can disable images

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ var duplexify = require('duplexify')
 
 var style = fs.readFileSync(__dirname+'/style.css', 'utf-8')
 
-module.exports = function() {
+module.exports = function(opts) {
   var render = phantom()
   var pack = tar.pack()
   var parse = ndjson.parse()
 
   parse.pipe(through.obj(function(data, enc, cb) {
     var body = marked(data.readme)
+    if (opts && opts.images === false) body = body.replace(/<img[^>]+>/g, '')
+
     var html = util.format('<html><head><style>%s</style><body class="markdown-body" style="background-color: #fff; padding: 25px;">%s</body></html>', style, body)
     var r = render()
 
