eecb3912ee0f9d3959e508cb243c152b16f1f7bd
Mathias Buus <mathiasbuus@gmail.com>
2014-11-02 10:30:24 +1000

    support --no-images

--- bin.js
+++ bin.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 var render = require('./')
+var images = process.argv.indexOf('--no-images') === -1
 
-process.stdin.pipe(render()).pipe(process.stdout)
+process.stdin.pipe(render({images:images})).pipe(process.stdout)
